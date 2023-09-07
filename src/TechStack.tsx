b34666d4cac242657332e415718d6c7815877070
laszloekovacs <laszloekovacs@gmail.com>
2023-09-07 09:43:42 +0200

    added tech list

--- src/TechStack.tsx
+++ src/TechStack.tsx
@@ -1,7 +1,8 @@
 import useSWR from 'swr'
 
 const fetcher = (url: string) => fetch(url).then((res) => res.json())
-const contentUrl = 'https://raw.githubusercontent.com/laszloekovacs/portfolio/main/data/content.json'
+const contentUrl =
+  'https://raw.githubusercontent.com/laszloekovacs/portfolio/main/data/content.json'
 
 const TechStack = () => {
   const { data, error, isLoading } = useSWR(contentUrl, fetcher)
@@ -9,6 +10,14 @@ const TechStack = () => {
   return (
     <div>
       <h2>Technology I'm using</h2>
+
+      {isLoading && <p>Loading...</p>}
+      {error && <p>Failed to load content</p>}
+
+      <ul>
+        {data &&
+          data.techStack.map((tech: any) => <li key={tech.id}>{tech.name}</li>)}
+      </ul>
     </div>
   )
 }
